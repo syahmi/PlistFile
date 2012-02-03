823641564503304d088db78d7939a1b7bddb9a02
Syahmi Ismail <kampongboy92@live.com>
2012-02-03 21:55:33 +0800

    Added StatesTableViewController

---
+++ PlistFile/StatesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  StatesTableViewController.h
+//  PlistFile
+//
+//  Created by Muhammad Syahmi Ismail on 2/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StatesTableViewController : UITableViewController
+
+@end
